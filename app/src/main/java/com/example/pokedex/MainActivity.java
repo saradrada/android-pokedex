1e59136735c5bb12e47331e477d7bedf53882a55
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-04-26 15:30:15 -0500

    Id's and getter of UI attributes

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -3,12 +3,110 @@ package com.example.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView pokeImageIV;
+    private TextView pokeNameTV;
+    private TextView pokeTypeTV;
+    private TextView pokeHab1TV;
+    private TextView pokeHab2TV;
+    private TextView pokeHab3TV;
+    private TextView pokeHab4TV;
+    private TextView pokeSpeedTV;
+    private TextView pokeDefenseTV;
+    private TextView pokeAttackTV;
+    private TextView pokeHpTV;
+
+    private TextView myPokemonsTV;
+    private EditText nameOrIDET;
+    private Button catchBtn;
+    private Button searchBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pokeImageIV = findViewById(R.id.pokeImageIV);
+        pokeTypeTV = findViewById(R.id.pokeTypeTV);
+        pokeHab1TV = findViewById(R.id.pokeHab1TV);
+        pokeHab2TV = findViewById(R.id.pokeHab2TV);
+        pokeHab3TV = findViewById(R.id.pokeHab3TV);
+        pokeHab4TV = findViewById(R.id.pokeHab4TV);
+        pokeSpeedTV  = findViewById(R.id.pokeSpeedTV);
+        pokeDefenseTV = findViewById(R.id.pokeDefenseTV);
+        pokeAttackTV = findViewById(R.id.pokeAttackTV);
+        pokeHpTV = findViewById(R.id.pokeHpTV);
+
+        myPokemonsTV = findViewById(R.id.myPokemosTV);
+        nameOrIDET = findViewById(R.id.nameOrIDET);
+        catchBtn = findViewById(R.id.catchBtn);
+        searchBtn = findViewById(R.id.searchBtn);
+    }
+
+    public ImageView getPokeImageIV() {
+        return pokeImageIV;
+    }
+
+    public TextView getPokeNameTV() {
+        return pokeNameTV;
+    }
+
+    public TextView getPokeTypeTV() {
+        return pokeTypeTV;
+    }
+
+    public TextView getPokeHab1TV() {
+        return pokeHab1TV;
+    }
+
+    public TextView getPokeHab2TV() {
+        return pokeHab2TV;
+    }
+
+    public TextView getPokeHab3TV() {
+        return pokeHab3TV;
+    }
+
+    public TextView getPokeHab4TV() {
+        return pokeHab4TV;
+    }
+
+    public TextView getPokeSpeedTV() {
+        return pokeSpeedTV;
+    }
+
+    public TextView getPokeDefenseTV() {
+        return pokeDefenseTV;
+    }
+
+    public TextView getPokeAttackTV() {
+        return pokeAttackTV;
+    }
+
+    public TextView getPokeHpTV() {
+        return pokeHpTV;
+    }
+
+    public TextView getMyPokemonsTV() {
+        return myPokemonsTV;
+    }
+
+    public EditText getNameOrIDET() {
+        return nameOrIDET;
+    }
+
+    public Button getCatchBtn() {
+        return catchBtn;
+    }
+
+    public Button getSearchBtn() {
+        return searchBtn;
     }
 }
